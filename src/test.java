9d93cd943033098b4e95b4d20626b9ccb3521ad1
MoonDoorKing <rizingsniper@kakao.com>
2022-12-03 13:42:40 +0900

    null 오류

---
+++ src/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        String name = null;
+        if(name==null) {
+            System.out.println("name 을 설정해주세요.");
+        }
+    }
+}
